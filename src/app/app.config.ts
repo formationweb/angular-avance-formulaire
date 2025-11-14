a4137cf902f290c9a9a8b646655e31e1c4e631c1
RSamaium <sam@newbot.io>
2025-11-14 16:34:54 +0100

    ngx translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideTranslateService({
+      lang: 'fr',
+      fallbackLang: 'en',
+      loader: provideTranslateHttpLoader({
+        prefix: 'i18n/',
+        suffix: '.json'
+      })
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, TranslateModule],
+  template: `
+    <h1>{{ 'welcome.title' | translate:{ name: 'sam' } }}</h1>
+    <button (click)="switchLang('fr')">fr</button>
+    <button (click)="switchLang('en')">en</button>
+
+    {{ lang().lang }}
+
+    <router-outlet />
+  `,
   styleUrl: './app.css'
 })
 export class App {
+  private translateService = inject(TranslateService)
   protected readonly title = signal('myapp');
+
+  constructor() {
+    effect(() => {
+      console.log(this.lang())
+    })
+  }
+
+  lang = toSignal(this.translateService.onLangChange, {
+    initialValue: {} as LangChangeEvent
+  })
+
+  switchLang(lang: string) {
+  
+    this.translateService.use(lang)
+  }
 }
